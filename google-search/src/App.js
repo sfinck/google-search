a2c64a6849d2104677d6b255e5c67142b91dde35
Sarah Finck <sarahfinck95@gmail.com>
2021-05-11 18:03:49 -0500

    changed results to title and added href link

--- google-search/src/App.js
+++ google-search/src/App.js
@@ -5,42 +5,49 @@ import './App.css';
 
 function App() {
 
-  const [book,setBook] = useState("");
-  const [result,setResult] = useState([]);
-  const [apiKey,setApiKey] = useState("AIzaSyAnE-y2_gr6tIfWt8LZqNRF7ioZeBGWxC8"); 
+  const [book, setBook] = useState("");
+  const [result, setResult] = useState([]);
+  const [apiKey, setApiKey] = useState("AIzaSyAnE-y2_gr6tIfWt8LZqNRF7ioZeBGWxC8");
 
-  function handleChange(event){
+  function handleChange(event) {
     const book = event.target.value;
     setBook(book);
   }
 
   function handleSubmit(event) {
     event.preventDefault();
-    axios.get("https://www.googleapis.com/books/v1/volumes?q="+book+"&key="+apiKey+"&maxResults=40")
-    .then(data => {
-      console.log(data.data.items);
-      setResult(data.data.items)
-    })
+    axios.get("https://www.googleapis.com/books/v1/volumes?q=" + book + "&key=" + apiKey + "&maxResults=40")
+      .then(data => {
+        console.log(data.data.items);
+        setResult(data.data.items)
+      })
   }
 
   return (
     <div className="App">
-        <h1>Google Search Application</h1>
-        <form onSubmit={handleSubmit}>
-          <div className ="form group">
-            <input type="text" onChange={handleChange}
-            className ="form-control mt-10" 
-            placeholder="search for books"
+      <h1>Google Search Application</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="form group">
+          <input type="text" onChange={handleChange}
+            className="form-control mt-10"
+            placeholder="Enter keyword"
             autoComplete="off"
-            />
-            <button type="submit" className="btn btn-danger">Search</button>
-          </div>
-        </form>
-       
+
+          />
+          <button type="submit" className="btn btn-danger">Search</button>
+        </div>
+      </form>
       {result.map(book => (
-        <img src={book.volumeInfo.imageLinks.thumbnail} alt={book.title}/>
+        <a target="_blank" href={book.volumeInfo.previewLink}>
+          <div className="resultCard">
+            <li>
+           {book.volumeInfo.title} 
+            </li>
+          {/* <img src={book.volumeInfo.imageLinks.thumbnail} alt={book.title}/> */}
+          </div>
+        </a>
       ))}
-      </div>
+    </div>
   );
 }
 
