f33f1cda21eb77d96d424d4a0a4ca68732b11ca3
Sarah Finck <sarahfinck95@gmail.com>
2021-05-11 15:55:00 -0500

    retrieved data from the API when searching

--- google-search/src/App.js
+++ google-search/src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
+import axios from "axios";
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 import './App.css';
 
 function App() {
-  
+
+  const [book,setBook] = useState("");
+  const [result,setResult] = useState([]);
+  const [apiKey,setApiKey] = useState("AIzaSyAnE-y2_gr6tIfWt8LZqNRF7ioZeBGWxC8"); 
+
   function handleChange(event){
- 
+    const book = event.target.value;
+    setBook(book);
   }
 
   function handleSubmit(event) {
-
+    event.preventDefault();
+    axios.get("https://www.googleapis.com/books/v1/volumes?q="+book+"&key="+apiKey+"&maxResults=40")
+    .then(data => {
+      console.log(data.data.items);
+    setResult(data.data.items)
+    })
   }
 
   return (
     <div className="App">
-      <div class="container">
         <h1>Google Search Application</h1>
         <form onSubmit={handleSubmit}>
-          <div class ="form group">
+          <div className ="form group">
             <input type="text" onChange={handleChange}
             className ="form-control mt-10" 
             placeholder="search for books"
@@ -26,7 +37,6 @@ function App() {
           </div>
         </form>
       </div>
-    </div>
   );
 }
 
