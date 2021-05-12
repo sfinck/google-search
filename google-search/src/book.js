bea38cdbb362f9ffded2759bebe1b0c74859c79d
Sarah Finck <sarahfinck95@gmail.com>
2021-05-11 23:15:19 -0500

    wrote code to navigate to saved books

--- google-search/src/book.js
+++ google-search/src/book.js
@@ -1,4 +1,5 @@
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
+// mongoose.connect('localhost:27017/test')
 // const Schema = mongoose.Schema;
 
 // const bookSchema = new Schema({
@@ -8,5 +9,31 @@
 // });
 
 // const Book = mongoose.model("Book", bookSchema);
+
+// router.get('/', function(req, res, next) {
+//   res.render('index');
+// });
+
+// router.get('/get-data', function(req, res, next) {
+//   Book.find()
+//   .then(function(doc) {
+//     res.render('index', {items:doc});
+//   });
+// }); 
+
+// router.post('/insert', function(req, res, next) {
+//   var item = {
+//     title: req.body.title,
+//     author: req.body.author,
+//     description: req.body.description
+//   };
+
+
+// var data = new Book(item);
+// data.save(); 
+
+// res.redirect('/');
+// }); 
+
 
 // module.exports = Book;
